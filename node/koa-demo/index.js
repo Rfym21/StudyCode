7828251df8d5c97ade4ded37c29dacec1336d1bc
Rfym21 <yumu@rfym.net>
2024-07-25 15:08:35 +0800

    up

---
+++ node/koa-demo/index.js
@@ -0,0 +1,14 @@
+
+const http = require('http')
+
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/home') {
+    res.end('<h1>Home Page</h1>')
+  } else {
+    res.end('NOT FOUND')
+  }
+
+}).listen(8201, () => {
+  console.log('Server is running at http://localhost:8201')
+})
