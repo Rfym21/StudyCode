ee172e23875b4084e85d18853b699872714e6646
Rfym21 <yumu@rfym.net>
2024-07-28 20:28:36 +0800

    update = 20160628.01  # 2016.06.28

--- node/v2api-service/index.js
+++ node/v2api-service/index.js
@@ -1,49 +1,163 @@
+// ---------------------------------以下为服务配置---------------------------------
 const koa = require('koa')
 const app = new koa()
 const cors = require('@koa/cors')
 const router = require('@koa/router')()
 const fs = require('fs')
 const path = require('path')
+const { base64Encode } = require('./lib/base64.js')
 
+
+// ---------------------------------以下为服务启动---------------------------------
 app.use(cors())
 app.use(router.routes())
 app.use(router.allowedMethods())
-
 app.listen(8103, () => {
   console.log('服务在端口8103上启动成功!')
 })
 
-router.prefix('/v2')
+// ---------------------------------以下为处理规则---------------------------------
+const worldRules = (content) => {
+  // 去除规则集合
+  const rules = [
+    "vmess://eyJhZGQiOiIxNjQuMTUyLjYwLjI0MiIsImFpZCI6IjAiLCJhbHBuIjoiIiwiZnAi",
+    "vmess://eyJhZGQiOiIxNjguMTM4LjEzMS4xMiIsImFpZCI6IjAiLCJhbHBuIjoiIiwiZnAi"
+  ]
 
-// 获取clash配置文件
-router.get('/clash', async ctx => {
-  ctx.body = getFIle("clash.yaml")
-})
-// 获取v2ray配置文件
-router.get('/v2ray', async ctx => {
-  ctx.body = getFIle("v2ray.txt")
-})
-// 获取自建配置文件
-router.get('/r', async ctx => {
-  ctx.body = getFIle("me.txt")
+  // 从节点中查找并去除包含规则的节点
+  for (let item of rules) {
+    const index = content.findIndex(rule => rule.includes(item))
+    if (index !== -1) {
+      content.splice(index, 1)
+    }
+  }
+  return content
+}
+
+const config = {
+  type: [
+    'v2ray',
+    'clash'
+  ],
+  file: {
+    v2ray: 'v2ray.txt',
+    me: 'me.txt'
+  },
+  name: {
+    world: worldRules
+  }
+}
+
+// ---------------------------------以下为路由配置---------------------------------
+// router.prefix('/v2')
+
+router.get('/', async ctx => {
+  // 获取 URL 查询字符串中的 type 和 name 参数
+  let { file, name, number, type } = URLToJson(ctx.url.slice(2))
+  // 获取相应的文件内容    
+  let content = []
+  if (file === 'all') {
+    for (item in config.file) {
+      const temp = getFIle(config.file[item])
+      content = [...temp, ...content]
+    }
+  } else {
+    content = getFIle(`${config.file[file]}`)
+    if (content === false) {
+      ctx.header['Content-Type'] = 'text/html'
+      ctx.body = '<h1 style="margin: 200px auto; width: 300px; text-align: center;font-size:48px;">请求参数错误</h1>'
+      return
+    }
+  }
+  const len = content.length
+  // Sub输出节点
+  let sub = []
+  // 查询有无 name 规则，如果有，进行进一步处理
+  if (name && config.name[name]) {
+    content = config.name[name](content)
+  }
+  // 判断 number 参数是否存在，如果存在，只返回指定数量的节点
+  if (number === 'all') {
+    number = content.length
+  }
+  else {
+    number = Number(number)
+    if (number) {
+      if (number <= 0) {
+        number = 1
+        // 从节点中查找并去除包含 sn://ssh? 的节点
+        const SnIndex = findIndexWithPrefix(content, 'sn://ssh?')
+        if (SnIndex !== -1) {
+          content.splice(SnIndex, 1)
+        }
+      }
+    } else {
+      // 如果 number 参数不存在，返回1个节点
+      number = 1
+      // 从节点中查找并去除包含 sn://ssh? 的节点
+      const SnIndex = findIndexWithPrefix(content, 'sn://ssh?')
+      if (SnIndex !== -1) {
+        content.splice(SnIndex, 1)
+      }
+    }
+
+    // 如果 number 大于节点数量，将 number 设置为节点数量
+    if (number > content.length) {
+      number = content.length
+    }
+  }
+
+  if (number !== len) {
+    // 只返回指定数量的节点
+    for (let i = 0; i < number; i++) {
+      sub.push(content.splice(Math.floor(Math.random() * content.length), 1)[0])
+    }
+  } else {
+    sub = content
+  }
+
+  // 分 type 进行处理
+  if (type === 'v2ray' || type === undefined) {
+    // 转为字符串，并对其进行 Base64 编码
+    sub = base64Encode(sub.join('\n'))
+    // 将编码后的字符串作为响应体返回
+    ctx.body = sub
+  }
+  else if (type === 'clash') {
+
+  }
+  else {
+    ctx.body = '<h1 style="margin: 200px auto; width: 300px; text-align: center;">暂不支持该类型</h1>'
+  }
+
+  function findIndexWithPrefix(arr, prefix) {
+    return arr.findIndex(item => item.startsWith(prefix))
+  }
 })
 
 
-/**
- * 根据文件名获取文件内容
- * @param {string} fileName - 要获取的文件名
- * @returns {string} - 文件内容，如果获取失败则返回错误信息
- */
-function getFIle(fileName) {
+// ---------------------------------以下为获取文件---------------------------------
+const getFIle = (fileName) => {
   // 使用 path.resolve() 方法创建文件的绝对路径
-  const File = path.resolve(__dirname, 'data', fileName);
-
+  const File = path.resolve(__dirname, 'data', fileName)
   try {
-    // 使用 fs.readFileSync() 方法同步读取文件内容，并指定编码为 utf-8
-    const content = fs.readFileSync(File, 'utf-8');
-    return content;
+    const fileContent = fs.readFileSync(File, 'utf-8')
+    const contentArray = fileContent.split(/\r?\n/).map(line => line.trim())
+    return contentArray
   } catch (e) {
     // 如果读取文件时出错，返回一段 HTML 代码表示服务暂停
-    return '<h1 style="margin: 200px auto; width: 300px; text-align: center;">暂停服务</h1>';
+    return false
   }
+}
+
+
+// ---------------------------------以下为URLToJson---------------------------------
+function URLToJson(str) {
+  const params = new URLSearchParams(str)
+  // 将解析后的参数转换为 JSON 对象
+  const jsonObject = {}
+  for (const [key, value] of params.entries()) {
+    jsonObject[key] = value
+  }
+  return jsonObject
 }
---
+++ vue/vue-v2api-client/src/views/SubView.vue
@@ -0,0 +1,117 @@
+<template>
+
+  <div
+    :class="`flex flex-col items-center justify-center h-screen py-20 ${mobile ? 'overflow-hidden' : 'overflow-y-auto'}`"
+    ref="page">
+    <ul class="w-5/6 mx-auto">
+      <li class="text-center">
+        <div class="mt-24 w-48 h-48 overflow-hidden rounded-full mx-auto"><img class="w-full" src="../assets/author.png"
+            alt=""></div>
+        <p class="text-2xl w-full mx-auto font-bold my-6 text-center">自定义一下参数吧!</p>
+        <p class="text-left text-sm w-full mx-auto mb-2"><span class="font-bold">文件:
+          </span>选择从哪个节点库获取节点,如果你不清楚请不要修改默认值!
+        </p>
+        <p class="text-left text-sm w-full mx-auto mb-2"><span class="font-bold">类型: </span>订阅链接的类型默认v2ray,暂时未适配Clash!
+        </p>
+        <p class="text-left text-sm w-full mx-auto mb-2"><span class="font-bold">规则: </span>规则参数,如果你没有获得专属的规则,请不要填写此项!
+        </p>
+        <p class="text-left text-sm w-full mx-auto mb-6"><span class="font-bold">数量:
+          </span>默认值为all,当填写的数量大于节点库节点数量时返回最大数量节点,不填或者不合法默认返回1个节点!</p>
+
+        <div
+          class="text-center flex align-middle w-full mx-auto bg-white bg-opacity-30 backdrop-filter backdrop-blur-md text-black rounded-full px-4 py-2 hover:bg-opacity-50 hover:shadow-lg transition duration-300 mb-4">
+          <p class="w-3/12 text-lg leading-10">File </p><input type="text" placeholder="请输入节点库参数!"
+            class="block flex-1 bg-white bg-opacity-30  backdrop-blur-md text-black rounded-full px-4 py-2  focus:outline-none"
+            v-model="file">
+        </div>
+
+        <div
+          class="text-center flex align-middle w-full mx-auto bg-white bg-opacity-30 backdrop-filter backdrop-blur-md text-black rounded-full px-4 py-2 hover:bg-opacity-50 hover:shadow-lg transition duration-300 mb-4">
+          <p class="w-3/12 text-lg leading-10">Type </p><input type="text" placeholder="请输入类型参数!"
+            class="block flex-1 bg-white bg-opacity-30  backdrop-blur-md text-black rounded-full px-4 py-2  focus:outline-none"
+            v-model="type">
+        </div>
+
+        <div
+          class="text-center flex align-middle w-full mx-auto bg-white bg-opacity-30 backdrop-filter backdrop-blur-md text-black rounded-full px-4 py-2 hover:bg-opacity-50 hover:shadow-lg transition duration-300 mb-4">
+          <p class="w-3/12 text-lg leading-10">Name </p><input type="text" placeholder="请输入规则参数!"
+            class="block flex-1 bg-white bg-opacity-30  backdrop-blur-md text-black rounded-full px-4 py-2  focus:outline-none"
+            v-model="name">
+        </div>
+
+        <div
+          class="text-center flex align-middle w-full mx-auto bg-white bg-opacity-30 backdrop-filter backdrop-blur-md text-black rounded-full px-4 py-2 hover:bg-opacity-50 hover:shadow-lg transition duration-300 mb-8">
+          <p class="w-3/12 text-lg leading-10">Number </p><input type="text" placeholder="请输入节点数量!"
+            class="block flex-1 bg-white bg-opacity-30  backdrop-blur-md text-black rounded-full px-4 py-2  focus:outline-none"
+            v-model="number">
+        </div>
+
+
+        <p
+          class="bg-black bg-opacity-30 backdrop-filter backdrop-blur-md text-black rounded-xl px-4 py-2 hover:bg-opacity-50 hover:shadow-lg transition duration-300 focus:outline-none h-20 mb-4 text-wrap overflow-hidden text-ellipsis">
+          {{ link }}
+        </p>
+
+        <button
+          class="bg-black bg-opacity-80 backdrop-filter backdrop-blur-md text-white rounded-xl px-20 py-2 hover:bg-opacity-50 hover:shadow-lg transition duration-300 focus:outline-none"
+          @click="copy">复制订阅链接
+        </button>
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+<script setup>
+import { ref, computed, onMounted, nextTick } from 'vue'
+import isMobileDevice from '@/lib/isMobileDevice'
+import BScroll from '@better-scroll/core'
+
+const page = ref(null)
+const host = "https://v2api.rfym.live"
+const file = ref('all')
+const type = ref('v2ray')
+const name = ref('')
+const number = ref('all')
+const mobile = ref(false)
+const link = computed(() => {
+  const res = []
+  if (file.value !== '') {
+    res.push(`file=${file.value}`)
+  }
+  if (type.value !== '') {
+    res.push(`type=${type.value}`)
+  }
+  if (name.value !== '') {
+    res.push(`name=${name.value}`)
+  }
+  if (number.value !== '') {
+    res.push(`number=${number.value}`)
+  }
+  return `${host}/?${res.join('&')}`
+})
+const copy = () => {
+  navigator.clipboard.writeText(link.value)
+}
+
+onMounted(() => {
+  if (isMobileDevice()) {
+    mobile.value = true
+    nextTick(() => {
+      new BScroll(page.value, {
+        click: true
+      })
+    })
+  }
+})
+
+</script>
+
+<style lang="css" scoped>
+ul,
+li {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+</style>
